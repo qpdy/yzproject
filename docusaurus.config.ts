cca103072b084037c5580997a08245ff9cd3a634
yangzhanpeng <446610985@qq.com>
2026-07-25 14:26:26 +0800

    fix: 修正 baseUrl 和 projectName 为正确的项目名称

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://qpdy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/yzpproject/',
+  baseUrl: '/yzproject/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'qpdy', // Usually your GitHub org/user name.
-  projectName: 'yzpproject', // Usually your repo name.
+  projectName: 'yzproject', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
